440316ca81f63e59c1582350bbbad9ebaf19e52a
Amar Teragundi <amarteragundi@gmail.com>
2019-04-08 15:16:24 +0530

    adding event listeners

--- app.js
+++ app.js
@@ -12,16 +12,24 @@ var UIController = (function(){
 
 //  Global App controller
 var controller = (function(budgetCtrl, UICtrl){
-	document.querySelector('.add__btn').addEventListener('click', function(){
-		// 1. get the field data from input.
-		
-		// 2. Add the item to the budget controller.
-		
-		// 3.Add the item to UI
-		
-		// 4. calculate the budget.
-		
-		// 5. display the budget on UI
-		
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+		var ctrlAddItem = function(){
+			// 1. get the field data from input.
+			
+			// 2. Add the item to the budget controller.
+			
+			// 3.Add the item to UI
+			
+			// 4. calculate the budget.
+			
+			// 5. display the budget on UI
+			
+		}
+
+	document.addEventListener('keypress', function(event){
+		if(event.keyCode == 13 || event.which == 13){
+			ctrlAddItem()
+		}
 	});
+
 })(budgetController, UIController);
